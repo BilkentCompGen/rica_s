b1422081373452d80b96ebbefc3f3ab16f23ebe1
Ricardo Roman-Brenes <ricardo@bilkent.edu.tr>
2026-07-02 15:19:14 +0300

    moving to /opt

--- scripts/AllClassify.sh
+++ scripts/AllClassify.sh
@@ -2,19 +2,26 @@
 
 echo "[i]> Begin of classification stage"
 
+
+#projecthome is a dir in the local fs
 projecthome=/home/ricardo/projects/rica_s/
 runid=$1
+
+#inputfile is a fasta/q in the docker filesystem
 inputfile="$2"
 outdir=$projecthome/output/$runid/
 
 mkdir -p $outdir
-
+echo "========================="
+echo $inputfile
+echo "========================="
 # for dir in ` ls -d /rica_s/scripts/rica_s_id_*/`
 for container in `ls $projecthome/scripts/ | grep rica_s_id*`
 do
     docker exec -it $container /rica_s/scripts/$container/classify.sh $inputfile /rica_s/output/$runid/ 2>&1 | tee -a $outdir/$runid.log
     printf "\n\n" | tee -a $outdir/$runid.log
-    # read -n 1 -p Continue?;
+    /home/ricardo/projects/rica_s/scripts/$container/fixOutput.sh
+    read -n 1 -p Continue?;
 done
 
 echo "[i]> End of classification stage"
--- scripts/rica_s_id_blast/classify.sh
+++ scripts/rica_s_id_blast/classify.sh
@@ -3,7 +3,9 @@
 
 echo "[i]> === blast"
 date
-
+echo $1
+echo $2
+echo $3
 
 
 inputfile="$1"
@@ -13,9 +15,10 @@ outdir="$2"
 blastdbfile="/rica_s/tools/rica_s_id_blast/pathogen_references.fasta.blastdb"
 
 
-blastn -query "$1" -db "$blastdbfile" -num_threads `nproc` -outfmt 0 > "$outdir/$outputfile"
-/usr/bin/time -v blastn -num_threads `nproc` -query $inputfile -db $blastdbfile -outfmt 0 > $outdir/$outputfile
-
+# blastn -query "$1" -db "$blastdbfile" -num_threads `nproc` -outfmt 0 > "$outdir/$outputfile"
+/usr/bin/time -v blastn -num_threads `nproc` -query $inputfile -db $blastdbfile -outfmt 0 > $outdir/$outputfile.0
+/usr/bin/time -v blastn -num_threads `nproc` -query $inputfile -db $blastdbfile -outfmt 6 > $outdir/$outputfile.6
+/usr/bin/time -v blastn -num_threads `nproc` -query $inputfile -db $blastdbfile -outfmt 18 > $outdir/$outputfile.18
 
 
 date
